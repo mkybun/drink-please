8cb64a017d96253d66bfd0297f0b75d0fa306c10
Karina Zuniga <mkybun@gmail.com>
2021-12-06 13:01:22 -0600

    homepage css

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div>
-      <h1 className="outer-drink">Drink Please</h1>
-      <Link to="random">Random Drink</Link>
-      <Link to="searchIngredient">Search By Ingredient</Link>
+      <div className="outer-drink">
+        <p className="drink-please">Drink Please</p>
+      </div>
+      <div className="routes-container">
+        <Link to="random">Random Drink</Link>
+        <Link to="searchIngredient">Search By Ingredient</Link>
+      </div>
     </div>
   );
 }
